f8faf3a6d70b6f314d0d802e488a1f075663e991
Saetbyul Ju <sbju@digitalmedic.ai>
2024-06-20 08:08:08 +0900

    Initial Commit

---
+++ SPMTest/SPMTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPMTestApp.swift
+//  SPMTest
+//
+//  Created by digitalmedic on 6/20/24.
+//
+
+import SwiftUI
+
+@main
+struct SPMTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
