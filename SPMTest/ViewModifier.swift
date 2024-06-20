baefddd00e36749372c0a842fa0f339cb769b7e3
Saetbyul Ju <sbju@digitalmedic.ai>
2024-06-20 09:33:19 +0900

    add view modifier

---
+++ SPMTest/ViewModifier.swift
@@ -0,0 +1,30 @@
+//
+//  ViewModifier.swift
+//  SPMTest
+//
+//  Created by digitalmedic on 6/20/24.
+//
+
+import Foundation
+import SwiftUI
+
+public struct LinearGradientBackground: ViewModifier {
+    let gradient: LinearGradient
+    let opacity: Double
+    public func body(content: Content) -> some View {
+        content.background(
+            gradient
+                .opacity(opacity)
+                .edgesIgnoringSafeArea(.all)
+        )
+    }
+}
+
+extension View {
+/// Generic background function for scenes
+    func linearGradientBackground(_ gradient: LinearGradient = LinearGradient(colors: [.red, .pink], startPoint: .top, endPoint: .bottom), _ opacity: Double = 1.0) -> some View {
+        self
+            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+            .modifier(LinearGradientBackground(gradient: gradient, opacity: opacity))
+    }
+}
